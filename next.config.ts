7cc8967c6c4884d1b84cb837cf63e3d09c300bea
Asmit tyagi <ashmittyagigms@gmail.com>
2025-02-02 08:46:42 +0530

    Refactor Question View Increment Logic ( Using after() of Next.js )

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ["pino", "pino-pretty"],
+  experimental: {
+    after: true,
+  },
   images: {
     remotePatterns: [
       {
